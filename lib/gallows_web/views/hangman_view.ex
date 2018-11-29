87681e368ac529f34de9ebd80924d957fba567df
Antonio Britto <antonioabritto@gmail.com>
2018-11-28 23:13:26 -0200

    add a cool image and logic to display it accordingly with the guesses

--- lib/gallows_web/views/hangman_view.ex
+++ lib/gallows_web/views/hangman_view.ex
@@ -12,4 +12,12 @@ defmodule GallowsWeb.HangmanView do
   def new_game_button(conn) do
     button("New Game", to: Routes.hangman_path(conn, :create_game))
   end
+
+  def turn(left, target) when target >= left do
+    "opacity: 1"
+  end
+
+  def turn(left, target) do
+    "opacity: 0.1"
+  end
 end
